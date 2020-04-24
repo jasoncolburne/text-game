b4596c3a8c9ca1ce725d8441f09c1573e4192ed2
Jason Colburne <jason@redbeardenterprises.com>
2020-04-23 23:47:40 -0400

    cleanup (#5)

--- text_game/combat_engine.py
+++ text_game/combat_engine.py
@@ -33,16 +33,20 @@ class CombatEngine:
 
     def player_turn(self, player: Character, enemy: Character) -> None:
         print("Your turn to attack!")
+
         valid_attacks = self._get_valid_attacks(player)
         attack = self.text_engine.menu(valid_attacks, "Your choice?")
+
         self._execute_combat(attack, player, enemy)
 
     def enemy_turn(self, player: Character, enemy: Character) -> None:
         print(f"It's the {enemy.name}'s turn!")
+
         valid_attacks = self._get_valid_attacks(enemy)
         number_of_attacks = len(valid_attacks)
         attack_index = randint(0, number_of_attacks - 1)
         attack = valid_attacks[attack_index]
+
         self._execute_combat(attack, enemy, player)
 
     def _get_valid_attacks(self, character: Character) -> List[str]:
@@ -54,8 +58,10 @@ class CombatEngine:
     def _execute_combat(self, attack_name: str, attacker: Character, defender: Character) -> None:
         attack = self.attacks[attack_name]
         attacker.consume_mana(attack['mana_required'])
+
         attack_roll = self._sum_dice(attack['attack_roll'])
         defense_roll = self._sum_dice(defender.defense)
+
         if attack_roll > defense_roll:
             damage = self._sum_dice(attack['damage_roll'])
             defender.damage(damage)
@@ -66,22 +72,19 @@ class CombatEngine:
 
     def _sum_dice(self, expression: str) -> int:
         match = DICE_RE.match(expression)
-        base_damage = int(match.group(1))  # type: ignore
+
+        base = int(match.group(1))  # type: ignore
         number_of_dice = int(match.group(2))  # type: ignore
         number_of_sides = int(match.group(3))  # type: ignore
-        return base_damage + sum(self._roll(number_of_dice, number_of_sides), 0)
+
+        return base + sum(self._roll(number_of_dice, number_of_sides), 0)
 
     def _roll(self, number: int, sides: int) -> List[int]:
-        result = []
         if sides not in ALLOWED_SIDES:
             raise AttributeError('Disallowed number of sides')
+
+        result = []
         for _ in range(number):
             result.append(randint(1, sides))
+
         return result
-
-
-# world = World()
-# world.enter()
-
-#         goblin = world.spawn('Goblin')
-#         CombatEngine(world.character, goblin).fight()
